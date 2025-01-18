45f4b69eb74480eeb39ceecf0dfe89f0526bd688
lbrooks81 <143453730+lbrooks81@users.noreply.github.com>
2025-01-18 15:43:12 -0500

    Still have to fix an issue with the # of guesses input

--- src/components/GuessingGame.vue
+++ src/components/GuessingGame.vue
@@ -25,7 +25,7 @@
       <div class="input-group">
         <!-- TODO users can still enter non-number data types despite the type of this input being number???-->
         <input class="form-control controls" id="guesses-remaining"
-               min="0" type="number" v-model="guessesRemaining" @input="isNumber($event)">
+               min="0" type="number" v-model.number="guessesRemaining" @input="isNumber($event)">
         <button class="btn btn-primary controls" id="confirm-guesses-btn" type="button"
                 @click="confirmNumberOfGuesses">
           Confirm
@@ -130,7 +130,19 @@ export default {
     },
     isNumber(event)
     {
+      //TODO This by all accounts should work but it doesn't
+      let inputString = event.target.value;
+      console.log(`Initial input string: ${inputString}`);
 
+      //* Slice off invalid characters
+      if(isNaN(parseInt(event.data)))
+      {
+        event.target.value = Number(inputString.slice(0, -1));
+        console.log("inputString after slice: ", inputString);
+        return;
+      }
+      //* Reassign value
+      event.target.value = inputString;
     }
   },
 
